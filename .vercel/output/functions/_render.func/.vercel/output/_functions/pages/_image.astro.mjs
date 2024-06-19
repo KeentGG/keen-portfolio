da7ecd35916cba414f21e767784c25c8bbd45967
Keanu Kent B. Gargar <keanukent.dev@gmail.com>
2024-06-20 01:35:45 +0800

    delete .vercel

--- .vercel/output/functions/_render.func/.vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('../chunks/generic_TkvgvGOB.mjs');
-
-export { page };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/pages/index.astro.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('../chunks/index_9zrl3qQI.mjs');
-
-export { page };
