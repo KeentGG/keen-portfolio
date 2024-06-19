ba49ca704369aecfc2da0471bc53c542e7654e0f
Keanu Kent B. Gargar <keanukent.dev@gmail.com>
2024-06-20 00:24:58 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 
-import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    nesting: true
-  }), react()]
+  integrations: [tailwind(), react()],
 });
